17cc8e7a473d33a4a3f080a219335ea63dc2c841
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2024-02-04 11:52:07 +0100

    Import only the nvidia module without prime

--- focus/m2/gen1/default.nix
+++ focus/m2/gen1/default.nix
@@ -1,9 +1,10 @@
-{
+{ lib, ... }: {
   imports = [
     ../../../common/cpu/intel
     ../../../common/pc/laptop
     ../../../common/pc/laptop/acpi_call.nix
     ../../../common/pc/laptop/ssd
+    ../../../common/gpu/nvidia
   ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
@@ -14,4 +15,15 @@
   ];
   boot.blacklistedKernelModules = [ "i2c_nvidia_gpu" ];
 
+  hardware.nvidia.modesetting.enable = lib.mkDefault true;
+  hardware.opengl = {
+    enable = lib.mkDefault true;
+    driSupport = lib.mkDefault true;
+    driSupport32Bit = lib.mkDefault true;
+  };
+
+  hardware.nvidia.prime = {
+    intelBusId = lib.mkDefault "PCI:0:2:0";
+    nvidiaBusId = lib.mkDefault "PCI:1:0:0";
+  };
 }
